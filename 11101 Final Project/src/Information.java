e18903b9c6bd776be81cddcfeea0fb23483af290
chienstarry <111306031@nccu.edu.tw>
2023-01-11 13:01:53 +0800

    add codes

--- 11101 Final Project/src/Information.java
+++ 11101 Final Project/src/Information.java
@@ -1,9 +1,16 @@
+import java.util.File;
+import java.util.Scanner;
+public class Information extends Personality{
+	private String introduction = "";
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Information(String name, String personality) {
+		super(name, personality);
 	}
 
+	public String Stringformat(){
+		Scanner input = new Scanner(new File("introduction.txt"));
+		
+		String result = String.format("%d, you are a %d. %d", name, personality, introduction);
+		return result;
+	}
 }
--- 11101 Final Project/src/Main.java
+++ 11101 Final Project/src/Main.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Sacnner(System.in);
+		System.out.println("Welcome to the MBTI test! Please answer the following questions to know yourself better.");
+		System.out.println("Please input your name: ");
+		Personality info = new Personality();
+		info.setName(in.nextLine());
 
+		System.out.println("Thank you for participating in this test!");
 	}
 
 }
